f5d0b07cf518c59a5862583094ecd27d9dc6d8cc
Iori Ota <sales_support@winbridge.biz>
2021-09-15 07:41:23 +0900

    Update and rename okurite.py to okurite_rev2.py
    ・ Add email address to dedicated index
    ・ Set a filter that does not require acquisition
    ・ Change the order of scraping (acquire customer information page by page)

--- okurite_rev2.py
+++ okurite_rev2.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Tue Sep 14 17:46:36 2021
+
+@author: iorin
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Thu Sep  2 21:00:20 2021
 
 @author: iorin
@@ -57,7 +64,7 @@ wb = gc.open_by_key(SPREADSHEET_KEY)
 #ws1 = wb.worksheet('sheet1')
 #ws2 = wb2.worksheet('imc-pager__item')
 ws3 = wb.worksheet('okurite')
-ws3.clear()
+#ws3.clear()
 
 
 #使用者ログイン
@@ -72,8 +79,8 @@ time.sleep(2)
 #Okuriteリスト（所要時間計測開始）
 start = time.time()
 
-td_urls = []
-for k in tqdm(range(1, 101)):
+#50ページまでスクレイピング
+for k in tqdm(range(1, 51)):
   try:  
     driver.get("https://tcare.pro/okurite?page=" + str(k))
     time.sleep(3)
@@ -103,23 +110,43 @@ for k in tqdm(range(1, 101)):
      continue
 
 #ヘッダを除くtrタグ数
-  print(len(soup.find_all('tr'))-1)
+  print(len(soup.find_all('tr'))-2)
 
 #①trタグに含まれるtdタグは8個なのでその分の積算を行う。
 #②増分を「8」に設定（「URL1」「URL2」「ステータス」「Created at」「編集/削除」を含めない為）
 #③lenにはヘッダー分とインデックスが0番目から始めることに注意し、取得データ数合わせの為に「-2」を行っている。
 #（参考リンク）https://atmarkit.itmedia.co.jp/ait/articles/1904/19/news027.html
-  for l in range(1, (len(soup.find_all('tr'))-2)*8, 8):
-        for td in soup.find_all('td')[l]:
-            print(td.get('href'))
-            td_urls.append(td.get('href'))
-  print(td_urls)
-  print(len(td_urls))    
     
+  td_urls = []
+  td_mails = []    
+  for l in range(1, (len(soup.find_all('tr'))-2)*8, 8):
+        for td in soup.find_all('td')[l+1]:
+#         print(td)
+         if "townpage" in td \
+             or "map.goo" in td \
+                 or "googl" in td \
+                    or "itp.ne.jp" in td \
+                        or "mapio" in td \
+                            or "navitime" in td:
+          pass
+         else:
+          for td in soup.find_all('td')[l]:
+            td_urls.append(td.get('href'))
+            print(td.get('href'))
+          for td in soup.find_all('td')[l+3]:
+            td_mails.append(td)
+            print(td)
+  print(td_urls)
+  print(len(td_urls))
+  print(td_mails)
+  print(len(td_mails))    
+  
 #ws1の行数を取得
   lastrow1 = len(ws3.col_values(1))
   print(lastrow1)       
-           
+
+
+#顧客情報リンク一覧           
   if lastrow1 == 0:
             cell_list2 = ws3.range(1, 1, str(len(td_urls)), 1)
 
@@ -128,35 +155,50 @@ for k in tqdm(range(1, 101)):
              c2.value = v2
              time.sleep(1)
              ws3.update_cells(cell_list2)
-            
+#            ws3.batch_update([
+#                {
+#                'range': cell_list2, 
+#                'values': [td_urls],
+#                }])            
   elif lastrow1 > 0:
-            cell_list2 = ws3.range(lastrow1+1, 1, lastrow1+1+len(td_urls), 1)
-            time.sleep(5)
-            for v2,c2 in zip(td_urls,cell_list2):
+            cell_list3 = ws3.range(lastrow1+1, 1, lastrow1+1+len(td_urls), 1)
+            for v2,c2 in zip(td_urls,cell_list3):
+             c2.value = v2
+             time.sleep(1)
+             ws3.update_cells(cell_list3)
+
+
+#メールアドレス一覧
+  if lastrow1 == 0:
+            cell_list2 = ws3.range(1, 6, str(len(td_mails)), 6)
+
+#スプレッドシートに入力
+            for v2,c2 in zip(td_mails,cell_list2):
              c2.value = v2
              time.sleep(1)
              ws3.update_cells(cell_list2)
+#            ws3.batch_update([
+#                {
+#                'range': cell_list2, 
+#                'values': [td_mails],
+#                }])            
+  elif lastrow1 > 0:
+            cell_list3 = ws3.range(lastrow1+1, 6, lastrow1+1+len(td_mails), 6)
+            for v2,c2 in zip(td_mails,cell_list3):
+             c2.value = v2
+             time.sleep(1)
+             ws3.update_cells(cell_list3)
 
-# calculate elapsed time
-elapsed_time = int(time.time() - start)
 
-# convert second to hour, minute and seconds
-elapsed_hour = elapsed_time // 3600
-elapsed_minute = (elapsed_time % 3600) // 60
-elapsed_second = (elapsed_time % 3600 % 60)
-
-# print as 00:00:00
-print("所要時間：" + str(elapsed_hour).zfill(2) + "h" \
-      + str(elapsed_minute).zfill(2) + "m" + str(elapsed_second).zfill(2) + "s")
-
-
-#顧客情報（所要時間計測開始）
-start = time.time()
-
+#顧客情報
 #ws1の行数を取得
-lastrow1 = len(ws3.col_values(1))
-print(lastrow1)       
-for k in tqdm(range(1, lastrow1+1)):
+  lastrow1 = len(ws3.col_values(1))
+  print(lastrow1)
+  lastrow2 = len(ws3.col_values(2))
+  print(lastrow2)
+  
+#  if lastrow2 == 0:
+  for k in range(lastrow2+1, lastrow1+1):
               try:
                driver.get('https://tcare.pro' + ws3.cell(k,1).value)
                time.sleep(5)
@@ -207,16 +249,9 @@ for k in tqdm(range(1, lastrow1+1)):
                     
 #Webサイト
               for td4 in soup.find_all('td')[6]:
-                  if "townpage" in td4.get('href') \
-                      or "map.goo.ne.jp" in td4.get('href') \
-                          or "google.com/search" in td4.get('href') \
-                              or "mapion" in td4.get('href') \
-                                  or "itp.ne.jp" in td4.get('href') \
-                                      or "navitime" in td4.get('href'):
-                   ws3.delete_row(k)
-                  else:
                    ws3.update_cell(k, 5, td4.get('href'))
                    print(td4.get('href'))
+   
 
 #chromeドライバーの終了
 driver.quit()
