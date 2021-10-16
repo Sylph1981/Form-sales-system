f294e11c94d1625deb533bb3598268a1887ae279
Iori Ota <sales_support@winbridge.biz>
2021-10-16 23:48:14 +0900

    Update by adding functions of the app.

--- app.py
+++ app.py
@@ -96,39 +96,49 @@ class MyFrame1 ( wx.Frame ):
 class MyFrame2 ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"営業支援ツール", pos = wx.DefaultPosition, size = wx.Size( 476,657 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"営業支援ツール update 2.0.2", pos = wx.DefaultPosition, size = wx.Size( 697,441 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		self.m_menubar1 = wx.MenuBar( 0 )
+		self.m_menubar1.SetFont( wx.Font( 10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
+
+		self.m_menu1 = wx.Menu()
+		self.m_menuItem1 = wx.MenuItem( self.m_menu1, wx.ID_ANY, u"MyMenuItem", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu1.Append( self.m_menuItem1 )
+
+		self.m_menuItem2 = wx.MenuItem( self.m_menu1, wx.ID_ANY, u"Exit", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu1.Append( self.m_menuItem2 )
+
+		self.m_menubar1.Append( self.m_menu1, u"File" )
+
+		self.m_menu11 = wx.Menu()
+		self.m_menuItem11 = wx.MenuItem( self.m_menu11, wx.ID_ANY, u"Configuration", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu11.Append( self.m_menuItem11 )
+
+		self.m_menuItem12 = wx.MenuItem( self.m_menu11, wx.ID_ANY, u"Profile", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu11.Append( self.m_menuItem12 )
+
+		self.m_menuItem13 = wx.MenuItem( self.m_menu11, wx.ID_ANY, u"Send text creation", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu11.Append( self.m_menuItem13 )
+
+		self.m_menubar1.Append( self.m_menu11, u"Setting" )
+
+		self.m_menu111 = wx.Menu()
+		self.m_menuItem111 = wx.MenuItem( self.m_menu111, wx.ID_ANY, u"Create", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu111.Append( self.m_menuItem111 )
+
+		self.m_menuItem211 = wx.MenuItem( self.m_menu111, wx.ID_ANY, u"MyMenuItem", wx.EmptyString, wx.ITEM_NORMAL )
+		self.m_menu111.Append( self.m_menuItem211 )
+
+		self.m_menubar1.Append( self.m_menu111, u"List" )
+
+		self.SetMenuBar( self.m_menubar1 )
 
 		mainSizer = wx.BoxSizer( wx.VERTICAL )
 
 		self.main_panel = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		mainBox = wx.BoxSizer( wx.VERTICAL )
-
-		hbox4 = wx.BoxSizer( wx.VERTICAL )
-
-		gSizer11 = wx.GridSizer( 0, 2, 0, 0 )
-
-		self.btn1 = wx.Button( self.main_panel, wx.ID_ANY, u"Create", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.btn1.SetFont( wx.Font( 10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
-
-		gSizer11.Add( self.btn1, 0, wx.ALL, 5 )
-
-		self.btn2 = wx.Button( self.main_panel, wx.ID_ANY, u"Exit", wx.Point( 1,-1 ), wx.DefaultSize, 0 )
-		self.btn2.SetFont( wx.Font( 10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
-
-		gSizer11.Add( self.btn2, 0, wx.ALL|wx.ALIGN_RIGHT, 5 )
-
-
-		hbox4.Add( gSizer11, 1, wx.EXPAND, 5 )
-
-		self.m_panel16 = wx.Panel( self.main_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		self.m_panel16.SetFont( wx.Font( 10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
-
-		hbox4.Add( self.m_panel16, 1, wx.EXPAND |wx.ALL, 5 )
-
-
-		mainBox.Add( hbox4, 1, wx.EXPAND, 5 )
 
 		bSizer19 = wx.BoxSizer( wx.VERTICAL )
 
@@ -139,16 +149,6 @@ class MyFrame2 ( wx.Frame ):
 
 
 		mainBox.Add( bSizer19, 1, wx.EXPAND, 5 )
-
-		hbox5 = wx.BoxSizer( wx.VERTICAL )
-
-		self.m_panel4 = wx.Panel( self.main_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		self.m_panel4.SetFont( wx.Font( 10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
-
-		hbox5.Add( self.m_panel4, 1, wx.EXPAND |wx.ALL, 5 )
-
-
-		mainBox.Add( hbox5, 1, wx.EXPAND, 5 )
 
 
 		self.main_panel.SetSizer( mainBox )
@@ -162,20 +162,8 @@ class MyFrame2 ( wx.Frame ):
 
 		self.Centre( wx.BOTH )
 
-		# Connect Events
-		self.btn1.Bind( wx.EVT_BUTTON, self.create_button )
-		self.btn2.Bind( wx.EVT_BUTTON, self.quit_button )
-
 	def __del__( self ):
 		pass
-
-
-	# Virtual event handlers, overide them in your derived class
-	def create_button( self, event ):
-		event.Skip()
-
-	def quit_button( self, event ):
-		event.Skip()
 
 
 ###########################################################################
@@ -203,7 +191,7 @@ class MyFrame5 ( wx.Frame ):
 class MyDialog ( wx.Dialog ):
 
 	def __init__( self, parent ):
-		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"リスト作成", pos = wx.DefaultPosition, size = wx.Size( 613,687 ), style = wx.DEFAULT_DIALOG_STYLE )
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"リスト作成", pos = wx.DefaultPosition, size = wx.Size( 517,656 ), style = wx.DEFAULT_DIALOG_STYLE )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
@@ -211,16 +199,6 @@ class MyDialog ( wx.Dialog ):
 
 		self.main_panel = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
 		mainBox = wx.BoxSizer( wx.VERTICAL )
-
-		bSizer30 = wx.BoxSizer( wx.VERTICAL )
-
-		self.m_panel22 = wx.Panel( self.main_panel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-		self.m_panel22.SetFont( wx.Font( 10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
-
-		bSizer30.Add( self.m_panel22, 1, wx.EXPAND |wx.ALL, 5 )
-
-
-		mainBox.Add( bSizer30, 1, wx.EXPAND, 5 )
 
 		hbox1 = wx.BoxSizer( wx.VERTICAL )
 
@@ -242,11 +220,6 @@ class MyDialog ( wx.Dialog ):
 
 		mainBox.Add( hbox4, 1, wx.EXPAND, 5 )
 
-		self.dbtn1 = wx.Button( self.main_panel, wx.ID_ANY, u"Exit", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.dbtn1.SetFont( wx.Font( 10, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
-
-		mainBox.Add( self.dbtn1, 0, wx.ALL|wx.ALIGN_RIGHT, 5 )
-
 
 		self.main_panel.SetSizer( mainBox )
 		self.main_panel.Layout()
@@ -259,16 +232,8 @@ class MyDialog ( wx.Dialog ):
 
 		self.Centre( wx.BOTH )
 
-		# Connect Events
-		self.dbtn1.Bind( wx.EVT_BUTTON, self.quit_button )
-
 	def __del__( self ):
 		pass
-
-
-	# Virtual event handlers, overide them in your derived class
-	def quit_button( self, event ):
-		event.Skip()
 
 
 ###########################################################################
@@ -278,7 +243,142 @@ class MyDialog ( wx.Dialog ):
 class MyFrame4 ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,594 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
+
+
+###########################################################################
+## Class MyDialog2
+###########################################################################
+
+class MyDialog2 ( wx.Dialog ):
+
+	def __init__( self, parent ):
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"User Authentication", pos = wx.DefaultPosition, size = wx.Size( 417,319 ), style = wx.DEFAULT_DIALOG_STYLE )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		bSizer17 = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_panel23 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.m_panel23.SetFont( wx.Font( 10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
+
+		sbSizer17 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel23, wx.ID_ANY, u"Please enter your account information." ), wx.VERTICAL )
+
+		self.m_panel20 = wx.Panel( sbSizer17.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.m_panel20.SetFont( wx.Font( 10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
+
+		sbSizer15 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel20, wx.ID_ANY, u"Username" ), wx.VERTICAL )
+
+		self.m_textCtrl4 = wx.TextCtrl( sbSizer15.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 200,-1 ), 0 )
+		sbSizer15.Add( self.m_textCtrl4, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+
+		self.m_panel20.SetSizer( sbSizer15 )
+		self.m_panel20.Layout()
+		sbSizer15.Fit( self.m_panel20 )
+		sbSizer17.Add( self.m_panel20, 1, wx.EXPAND |wx.ALL, 5 )
+
+		self.m_panel21 = wx.Panel( sbSizer17.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.m_panel21.SetFont( wx.Font( 10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
+
+		sbSizer16 = wx.StaticBoxSizer( wx.StaticBox( self.m_panel21, wx.ID_ANY, u"Password" ), wx.VERTICAL )
+
+		self.m_textCtrl5 = wx.TextCtrl( sbSizer16.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 200,-1 ), wx.TE_PASSWORD )
+		sbSizer16.Add( self.m_textCtrl5, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+
+		self.m_panel21.SetSizer( sbSizer16 )
+		self.m_panel21.Layout()
+		sbSizer16.Fit( self.m_panel21 )
+		sbSizer17.Add( self.m_panel21, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+		self.m_panel23.SetSizer( sbSizer17 )
+		self.m_panel23.Layout()
+		sbSizer17.Fit( self.m_panel23 )
+		bSizer17.Add( self.m_panel23, 1, wx.EXPAND |wx.ALL, 5 )
+
+		self.m_panel24 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		gSizer7 = wx.GridSizer( 0, 2, 0, 0 )
+
+		self.m_button10 = wx.Button( self.m_panel24, wx.ID_OK, u"Login", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_button10.SetFont( wx.Font( 10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
+
+		gSizer7.Add( self.m_button10, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+		self.m_button11 = wx.Button( self.m_panel24, wx.ID_CANCEL, u"Cancel", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_button11.SetFont( wx.Font( 10, wx.FONTFAMILY_MODERN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Meiryo UI" ) )
+
+		gSizer7.Add( self.m_button11, 0, wx.ALL, 5 )
+
+
+		self.m_panel24.SetSizer( gSizer7 )
+		self.m_panel24.Layout()
+		gSizer7.Fit( self.m_panel24 )
+		bSizer17.Add( self.m_panel24, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+		self.SetSizer( bSizer17 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
+
+
+###########################################################################
+## Class MyDialog3
+###########################################################################
+
+class MyDialog3 ( wx.Dialog ):
+
+	def __init__( self, parent ):
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Setting", pos = wx.DefaultPosition, size = wx.Size( 498,531 ), style = wx.DEFAULT_DIALOG_STYLE )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
+
+
+###########################################################################
+## Class MyDialog4
+###########################################################################
+
+class MyDialog4 ( wx.Dialog ):
+
+	def __init__( self, parent ):
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Setting of transmission contents", pos = wx.DefaultPosition, size = wx.Size( 853,464 ), style = wx.DEFAULT_DIALOG_STYLE )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
+
+
+###########################################################################
+## Class MyDialog5
+###########################################################################
+
+class MyDialog5 ( wx.Dialog ):
+
+	def __init__( self, parent ):
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Email body creation", pos = wx.DefaultPosition, size = wx.Size( 670,655 ), style = wx.DEFAULT_DIALOG_STYLE )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
